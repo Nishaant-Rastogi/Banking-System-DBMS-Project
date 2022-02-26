e94e38d8f65d5c551a0b4001cec6b5da9a51ab52
Nishaant-Rastogi <nishaant20091@iiitd.ac.in>
2022-02-26 11:12:45 +0530

    savings

--- DBMS data/savings_accounts.sql
+++ DBMS data/savings_accounts.sql
@@ -2,8 +2,10 @@ drop table Savings_Accounts;
 CREATE TABLE Savings_Accounts(
 AccountNo VARCHAR(100) NOT NULL PRIMARY KEY,
 Balance DOUBLE,
+TransactionLimit Double DEFAULT 10000,
 InterestRate int DEFAULT '6',
 FOREIGN KEY (AccountNo) REFERENCES Accounts(AccountNo)
 );
 
 INSERT INTO Savings_Accounts(AccountNo, Balance) SELECT AccountNo, Balance from Accounts where AccountNo like '______00____';
+UPDATE Savings_Account set TransactionLimit = IF(Balance <= 10000, Balance, 10000); 
