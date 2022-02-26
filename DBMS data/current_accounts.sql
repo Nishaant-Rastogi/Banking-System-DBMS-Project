869538047bf73a3ed203e479c3ed0607685923f1
Divyam Gupta <82924774+fruitkijin@users.noreply.github.com>
2022-02-26 21:23:04 +0530

    Cries

--- DBMS data/current_accounts.sql
+++ DBMS data/current_accounts.sql
@@ -2,7 +2,29 @@ drop table Current_Accounts;
 CREATE TABLE Current_Accounts(
 AccountNo VARCHAR(100) NOT NULL PRIMARY KEY,
 Balance DOUBLE,
+BusinessName VARCHAR(200),
 FOREIGN KEY (AccountNo) REFERENCES Accounts(AccountNo)
 );
 
 INSERT INTO Current_Accounts(AccountNo, Balance) SELECT AccountNo, Balance from Accounts where AccountNo like '______01____';
+INSERT INTO Current_Accounts(BusinessName) VALUES ('ABC');
+INSERT INTO Current_Accounts(BusinessName) VALUES ('QWE');
+INSERT INTO Current_Accounts(BusinessName) VALUES ('WER');
+INSERT INTO Current_Accounts(BusinessName) VALUES ('TRV');
+INSERT INTO Current_Accounts(BusinessName) VALUES ('IOL');
+INSERT INTO Current_Accounts(BusinessName) VALUES ('IOP');
+INSERT INTO Current_Accounts(BusinessName) VALUES ('LKJ');
+INSERT INTO Current_Accounts(BusinessName) VALUES ('ASD');
+INSERT INTO Current_Accounts(BusinessName) VALUES ('CVB');
+INSERT INTO Current_Accounts(BusinessName) VALUES ('BNM');
+INSERT INTO Current_Accounts(BusinessName) VALUES ('PLM');
+INSERT INTO Current_Accounts(BusinessName) VALUES ('QAZ');
+INSERT INTO Current_Accounts(BusinessName) VALUES ('SKY');
+INSERT INTO Current_Accounts(BusinessName) VALUES ('QWE');
+INSERT INTO Current_Accounts(BusinessName) VALUES ('OPA');
+INSERT INTO Current_Accounts(BusinessName) VALUES ('SDF');
+INSERT INTO Current_Accounts(BusinessName) VALUES ('DFG');
+INSERT INTO Current_Accounts(BusinessName) VALUES ('GHJ');
+INSERT INTO Current_Accounts(BusinessName) VALUES ('KHI');
+INSERT INTO Current_Accounts(BusinessName) VALUES ('SDF');
+INSERT INTO Current_Accounts(BusinessName) VALUES ('JIN');
--- DBMS data/customer_account_transaction.sql
+++ DBMS data/customer_account_transaction.sql
@@ -12,7 +12,7 @@ PRIMARY KEY(Customer_ID, Payment_ID, AccountNo)
 INSERT INTO Customer_Account_Transaction(Customer_ID, Payment_ID, AccountNo, Amount) 
 SELECT C.Customer_ID, Payment_ID, A.AccountNo, T.Amount 
 from Customers C, Accounts A, Transactions T 
-where substring(A.AccountNo,5,2) = '01' AND substring(A.AccountNo,3,2) = substring(Payment_ID,11,2) AND substring(A.AccountNo,11,2) = substring(Payment_ID,9,2)AND A.Customer_ID = C.Customer_ID;
+where substring(A.AccountNo,5,2) = '01' AND substring(A.AccountNo,3,2) = substring(Payment_ID,3,2) AND substring(A.AccountNo,11,2) = substring(Payment_ID,9,2)AND A.Customer_ID = C.Customer_ID;
 
 UPDATE Customer_Account_Transaction
 set Transaction_Type = 'Customer to Customer'
@@ -23,5 +23,14 @@ set Transaction_Type = 'Loan Payment'
 where substring(Payment_ID, 7, 2)= '02';
 
 UPDATE Customer_Account_Transaction
-set Recipient = 
-where substring(Payment_ID, 7, 2)= '01';
+set Recipient = concat(concat(concat(concat('100', substring(Payment_ID, 11, 2)), '01'),'000'),substring(Payment_ID, 14, 1))
+where substring(Payment_ID, 7, 2)= '01' AND substring(Payment_ID,18,1) = 'C';
+
+UPDATE Customer_Account_Transaction
+set Recipient = concat(concat(concat(concat('100', substring(Payment_ID, 11, 2)), '00'),'000'),substring(Payment_ID, 14, 1))
+where substring(Payment_ID, 7, 2)= '01' AND substring(Payment_ID,18,1) = 'S';
+
+UPDATE Customer_Account_Transaction
+set Recipient = AccountNo
+where Amount<0;
+
