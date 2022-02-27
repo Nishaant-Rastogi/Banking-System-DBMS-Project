ec64f0fea241b078b4c06c9fb60a47fd38a6014d
Divyam Gupta <82924774+fruitkijin@users.noreply.github.com>
2022-02-27 21:17:26 +0530

    Bank Name Change

--- DBMS data/branches.sql
+++ DBMS data/branches.sql
@@ -7,26 +7,26 @@ Address VARCHAR(100) NOT NULL
 INSERT INTO Branches(Branch_ID, Branch_Name, Address)
 VALUES
 (
-1001, 'DelBank Punjabi Bagh', 'Delhi'
+1001, 'DANK Punjabi Bagh', 'Delhi'
 );
 INSERT INTO Branches(Branch_ID, Branch_Name, Address)
 VALUES
 (
-1002, 'DelBank New Delhi', 'New Delhi'
+1002, 'DANK New Delhi', 'New Delhi'
 );
 INSERT INTO Branches(Branch_ID,Branch_Name, Address)
 VALUES
 (
-1003, 'DelBank Delhi-6', 'Delhi-6'
+1003, 'DANK Delhi-6', 'Delhi-6'
 );
 INSERT INTO Branches(Branch_ID, Branch_Name, Address)
 VALUES
 (
-1004, 'DelBank Connaught Place', 'New Delhi'
+1004, 'DANK Connaught Place', 'New Delhi'
 );
 INSERT INTO Branches(Branch_ID, Branch_Name, Address)
 VALUES
 (
-1005, 'DelBank Harkesh Nagar', 'Delhi'
+1005, 'DANK Harkesh Nagar', 'Delhi'
 );
 
