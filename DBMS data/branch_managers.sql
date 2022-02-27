76b1d4754c17e18f8d3997eaa87bc6c69286bc23
Divyam Gupta <82924774+fruitkijin@users.noreply.github.com>
2022-02-27 22:13:42 +0530

    Branch Manager Changes

--- DBMS data/branch_managers.sql
+++ DBMS data/branch_managers.sql
@@ -4,6 +4,9 @@ Employee_ID VARCHAR(100) NOT NULL,
 Branch_ID bigint NOT NULL,
 PRIMARY KEY (Employee_ID, Branch_ID),
 Name VARCHAR(100) NOT NULL,
-Designation VARCHAR(100) NOT NULL
+Designation VARCHAR(100) NOT NULL,
+Term int NOT NULL DEFAULT(0),
+Joining_Date DATE NOT NULL
 );
-INSERT INTO Branch_Manager(Employee_ID, Branch_ID, Name, Designation) SELECT Employee_ID, Branch_ID, Name, Designation from Employees, Branches where Designation like 'Branch Manager' AND (substring(Employee_ID, 4,1) = substring(Branch_ID,4,1));
+INSERT INTO Branch_Manager(Employee_ID, Branch_ID, Name, Designation, Joining_Date) SELECT Employee_ID, Branch_ID, Name, Designation, Joining_Date from Employees, Branches where Designation like 'Branch Manager' AND (substring(Employee_ID, 4,1) = substring(Branch_ID,4,1));
+UPDATE Branch_Manager set Term = IF(2022 - cast(YEAR(Joining_Date) as UNSIGNED)>10,2022 - cast(YEAR(Joining_Date) as UNSIGNED)-10, 2022 - cast(YEAR(Joining_Date) as UNSIGNED)) ;
