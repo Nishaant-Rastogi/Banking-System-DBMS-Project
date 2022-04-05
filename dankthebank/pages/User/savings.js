bd6c28c7ec0fc70de4d5ea1707e321b6ceaf00c4
Kushagra20075 <Kushagra20075@iiitd.ac.in>
2022-04-05 21:21:10 +0530

    USER PAGES ADDED

--- dankthebank/pages/User/savings.js
+++ dankthebank/pages/User/savings.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Userbar from '../../components/Userbar';
-import DropDown from '../../components/dropDown';
+import DropDown from '../../components/DropD';
 import Table from '../../components/Table';
 function savings() {
   return (
