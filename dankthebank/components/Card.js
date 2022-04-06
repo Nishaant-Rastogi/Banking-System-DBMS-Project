5ec1b7e21e6b61b50e888a73df08bb8fa4b0bf93
Kushagra20075 <Kushagra20075@iiitd.ac.in>
2022-04-06 23:38:53 +0530

    Some Work Done

--- dankthebank/components/Card.js
+++ dankthebank/components/Card.js
@@ -5,9 +5,10 @@ function CustomCard() {
         <div className="CUSTOMCARD" id="accordion">
             <div className="card">
                 <div className="card-header COL" id="headingOne">
+                <button className="btn btn-link BUTTON" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
                     <div className="DATA ACCOUNT">
                         <div className="HEAD">
-                            ACCOUNT NUMBER :
+                            Transaction Id :
                         </div>
                         <div className="VALUE">
                         123456789   
@@ -15,51 +16,117 @@ function CustomCard() {
                     </div>
                     <div className="DATA BALANCE">
                         <div className="HEAD">
-                            BALANCE :
+                            Amount:
                         </div>
                         <div className="VALUE">
-                            12000
+                            12000<image src="/greenup.png" className="VALUEIMG" />
+                            {/*<image src="/reddown.png" className="VALUEIMG"/>*/ }
                         </div>
                     </div>
-                    <div className="TOGGLE">
-                        <h5 className="mb-0">
-                            <button className="btn btn-link BUTTON" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                                V
-                            </button>
-                        </h5>
+                    <div className="DATA STATUS">
+                        <div className="HEAD">
+                            Status:
+                        </div>
+                        <div id="SUCCESS" className="VALUE">
+                            Successful
+                        </div>
+                        {/*
+                        <div id="FAILURE" className="VALUE">
+                            FAILED
+                        </div>
+                        */}
+                    </div>
+                    </button>
+                </div>
+                <div id="collapseOne" className="collapse" aria-labelledby="headingOne" data-parent="#accordion">
+                <div className="card-body CBODY">
+                <div className="DATA FROM">
+                        <div className="HEAD">
+                            FROM:
+                        </div>
+                        <div className="VALUE">
+                            123456789
+                        </div>
+                    </div>
+                    <div className="DATA TO">
+                        <div className="HEAD">
+                            TO:
+                        </div>
+                        <div className="VALUE">
+                            XDXDXDXDXXDXD
+                        </div>
+                    </div>
+                    <div className="DATA DATE">
+                        <div className="HEAD">
+                            Date :
+                        </div>
+                        <div className="VALUE">
+                        1 April 2022 
+                        </div>
                     </div>
                 </div>
-                <div id="collapseOne" className="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
-                <div className="card-body">
-                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-                </div>
                 </div>
             </div>
             <div className="card">
-                <div className="card-header" id="headingTwo">
-                <h5 className="mb-0">
-                    <button className="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                    Collapsible Group Item #2
+                <div className="card-header COL" id="headingTwo">
+                <button className="btn btn-link BUTTON" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseOne">
+                    <div className="DATA ACCOUNT">
+                        <div className="HEAD">
+                            Transaction Id :
+                        </div>
+                        <div className="VALUE">
+                        123456789   
+                        </div>
+                    </div>
+                    <div className="DATA BALANCE">
+                        <div className="HEAD">
+                            Amount:
+                        </div>
+                        <div className="VALUE">
+                            12000<image src="/greenup.png" className="VALUEIMG" />
+                            {/*<image src="/reddown.png" className="VALUEIMG"/>*/ }
+                        </div>
+                    </div>
+                    <div className="DATA STATUS">
+                        <div className="HEAD">
+                            Status:
+                        </div>
+                        {/* <div id="SUCCESS" className="VALUE">
+                            Successful
+                        </div> */}
+                        <div id="FAILURE" className="VALUE">
+                            FAILED
+                        </div>
+                       
+                    </div>
                     </button>
-                </h5>
                 </div>
                 <div id="collapseTwo" className="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
-                <div className="card-body">
-                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-                </div>
-                </div>
-            </div>
-            <div className="card">
-                <div className="card-header" id="headingThree">
-                <h5 className="mb-0">
-                    <button className="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                    Collapsible Group Item #3
-                    </button>
-                </h5>
-                </div>
-                <div id="collapseThree" className="collapse" aria-labelledby="headingThree" data-parent="#accordion">
-                <div className="card-body">
-                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                <div className="card-body CBODY">
+                <div className="DATA FROM">
+                        <div className="HEAD">
+                            FROM:
+                        </div>
+                        <div className="VALUE">
+                            123456789
+                        </div>
+                    </div>
+                    <div className="DATA TO">
+                        <div className="HEAD">
+                            TO:
+                        </div>
+                        <div className="VALUE">
+                            XDXDXDXDXXDXD
+                        </div>
+                    </div>
+                    <div className="DATA DATE">
+                        <div className="HEAD">
+                            Date :
+                        </div>
+                        <div className="VALUE">
+                        1 April 2022 
+                        </div>
+                    </div>
                 </div>
                 </div>
             </div>
